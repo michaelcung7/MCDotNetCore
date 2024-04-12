724c07472f66dd321f4fd5ab0a448d64c5933799
Michael Cung <cungmichael63@gmail.com>
2024-04-12 16:31:43 +0630

    add db connection

--- MCDotNetCore.ConsoleApp/Program.cs
+++ MCDotNetCore.ConsoleApp/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+using System.Data.SqlClient;
+
 Console.WriteLine("Hello, World!");
+SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+stringBuilder.DataSource = "DESKTOP-SO9GMR6"; //Server Name
+stringBuilder.InitialCatalog = "DotnetTrainingBatch4"; //DB Name
+ stringBuilder.UserID = "sa";
+stringBuilder.Password = "sa@123";
+SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+//SqlConnection connection = new SqlConnection(connection.Open()connection.Open();;);
+connection.Open();
+string query = "select * from tbl_blog";
+SqlCommand cmd = new SqlCommand(query, connection);
+SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+DataTable dt = new DataTable();
+sqlDataAdapter.Fill(dt);
+connection.Close();
+foreach( DataRow dr in dt.Rows)
+{
+    Console.WriteLine("Blog Id =>" + dr["BlogId"]);
+    Console.WriteLine("Blog Title =>" + dr["BlogTitle"]);
+    Console.WriteLine("Blog Author =>" + dr["BlogAuthor"]);
+    Console.WriteLine("Blog Content =>" + dr["BlogContent"]);
+}
+Console.ReadKey();
