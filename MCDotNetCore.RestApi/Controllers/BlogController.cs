5c548184c485263b8b428a0eb67979c77931b723
Michael Cung <cungmichael63@gmail.com>
2024-05-03 17:55:19 +0800

    Rest Api

---
+++ MCDotNetCore.RestApi/Controllers/BlogController.cs
@@ -0,0 +1,121 @@
+﻿using MCDotNetCore.Restapi.DB;
+using MCDotNetCore.RestApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MCDotNetCore.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+
+        private readonly AppDBContext _context;
+
+        public BlogController()
+        {
+            _context = new AppDBContext();
+        }
+        [HttpGet]
+        public IActionResult Read()
+        {
+            var lst = _context.Blogs.ToList();
+            return Ok(lst);
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Edit(int id)
+        {
+            var item = _context.Blogs.FirstOrDefault(x => x.BlogId == id);
+
+            if (item is null)
+            {
+                return NotFound("Item Not Found");
+
+            }
+            return Ok(item);
+
+        }
+
+        [HttpPost]
+        public IActionResult Create(BlogModel blog)
+        {
+            _context.Blogs.Add(blog);
+            int result = _context.SaveChanges();
+
+            string message = result > 0 ? "Saving Success" : "Saving Failed";
+            return Ok(message);
+
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, BlogModel blog)
+        {
+            var item = _context.Blogs.FirstOrDefault(x => x.BlogId == id);
+
+            if (item is null)
+            {
+                return NotFound("Item Not Found");
+
+            }
+            item.BlogTitle = blog.BlogTitle;
+            item.BlogAuthor = blog.BlogAuthor;
+            item.BlogContent = blog.BlogContent;
+            int result = _context.SaveChanges();
+
+            string message = result > 0 ? "Update Success" : "Update Failed";
+            return Ok(message);
+
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, BlogModel blog)
+        {
+            var item = _context.Blogs.FirstOrDefault(x => x.BlogId == id);
+
+            if (item is null)
+            {
+                return NotFound("Item Not Found");
+
+            }
+            if (!string.IsNullOrEmpty(blog.BlogTitle))
+            {
+                item.BlogTitle = blog.BlogTitle;
+            }
+            if (!string.IsNullOrEmpty(blog.BlogAuthor))
+            {
+                item.BlogAuthor = blog.BlogAuthor;
+            }
+            if (!string.IsNullOrEmpty(blog.BlogContent))
+            {
+                item.BlogContent = blog.BlogContent;
+            }
+
+            int result = _context.SaveChanges();
+
+            string message = result > 0 ? "Update Success" : "Update Failed";
+            return Ok(message);
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var item = _context.Blogs.FirstOrDefault(x => x.BlogId == id);
+
+            if (item is null)
+            {
+                return NotFound("Item Not Found");
+
+            }
+            _context.Blogs.Remove(item);
+            int result = _context.SaveChanges();
+
+            string message = result > 0 ? "Delete Success" : "Delete Failed";
+            return Ok(message); 
+
+
+        }
+    }
+}
---
+++ MCDotNetCore.RestApi/DB/AppDBContext.cs
@@ -0,0 +1,21 @@
+﻿using MCDotNetCore.RestApi;
+using MCDotNetCore.RestApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace MCDotNetCore.Restapi.DB
+{
+    internal class AppDBContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(ConnectionString.sqlConnectionStringBuilder.ConnectionString);
+        }
+        public DbSet<BlogModel> Blogs { get; set; }
+
+    }
+}
