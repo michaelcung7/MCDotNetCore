6f32da535f4ea479f71cc37dc7a58e41969d43b9
Michael Cung <cungmichael63@gmail.com>
2024-05-14 01:56:20 +0800

    LatHtaukBayDin Api

---
+++ MCDotNetCore.ConsoleAppHttpClient/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string jsonstr = await File.ReadAllTextAsync("data.json");
+Console.WriteLine(jsonstr);
+
+Console.ReadLine();
+
+
+public class MainDTO
+{
+    public Question[] questions { get; set; }
+    public Answer[] answers { get; set; }
+    public string[] numberList { get; set; }
+}
+
+public class Question
+{
+    public int questionNo { get; set; }
+    public string questionName { get; set; }
+}
+
+public class Answer
+{
+    public int questionNo { get; set; }
+    public int answerNo { get; set; }
+    public string answerResult { get; set; }
+}
---
+++ MCDotNetCore.RestApiWithNLayer/Features/LatHtaukBayDin/LatHtaukBayDinController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+namespace MCDotNetCore.RestApiWithNLayer.Features.LatHtaukBayDin
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LatHtaukBayDinController : ControllerBase
+    {
+
+        private async Task<LatHtaukBayDin> GetDataAsync()
+        {
+            string jsonstr = await System.IO.File.ReadAllTextAsync("data.json");
+            var model = JsonConvert.DeserializeObject<LatHtaukBayDin>(jsonstr);
+            return model;
+
+        }
+
+        [HttpGet("questions")]
+        public async Task<IActionResult> Questions()
+        {
+            var model = await GetDataAsync();
+            return Ok(model.questions);
+        }
+
+        [HttpGet("number-list")]
+        public async Task<IActionResult> GetNumberList()
+        {
+            var model = await GetDataAsync();
+            return Ok(model.numberList);
+
+        }
+
+        [HttpGet("answer/{questionNo}/{no}")]
+        public async Task<IActionResult> GetAnswer(int questionNo, int no)
+        {
+            var model = await GetDataAsync();
+            return Ok(model.answers.FirstOrDefault(x => x.questionNo == questionNo && x.answerNo == no ));
+
+        }
+
+
+        public class LatHtaukBayDin
+        {
+            public Question[] questions { get; set; }
+            public Answer[] answers { get; set; }
+            public string[] numberList { get; set; }
+        }
+
+        public class Question
+        {
+            public int questionNo { get; set; }
+            public string questionName { get; set; }
+        }
+
+        public class Answer
+        {
+            public int questionNo { get; set; }
+            public int answerNo { get; set; }
+            public string answerResult { get; set; }
+        }
+
+    }
+}
